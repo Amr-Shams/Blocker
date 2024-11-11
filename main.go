77332ba44a6f6275e3a20315e92dfa94319ffefc
Amr-Shams <amr.shams2015.as@gmail.com>
2024-11-11 12:37:31 +0200

    Create issues for the following todos: 14

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	// TODO: cmd update the pointer to the file
+	// TODO(14): cmd update the pointer to the file
 	// use a singleton pattern to avoid multiple instances for the db(filesystem)
 	cmd := cmd.NewRootCommand()
 	if err := cmd.Execute(); err != nil {
